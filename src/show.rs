110a9d52160abcdc895603ba2d0786e643ae1210
Tomas Sedovic <tomas@sedovic.cz>
2021-12-17 23:01:20 +0100

    Open a file on clicking

--- src/show.rs
+++ src/show.rs
@@ -1,4 +1,16 @@
+use std::path::Path;
+
 #[derive(Debug)]
 pub struct Show {
     pub name: String,
+}
+
+impl Show {
+    pub fn current_episode(&self) -> &Path {
+        Path::new("/home/shadower/Videos/Bleach/Bleach - 01.mkv")
+    }
+
+    pub fn next(&mut self) {
+        todo!()
+    }
 }
--- src/window.rs
+++ src/window.rs
@@ -15,10 +15,14 @@ pub fn show(state: &mut State, ui: &mut egui::Ui) {
 
     if ui.button("Play Next").clicked() {
         println!("Clicked: Playing next");
-        // TODO: https://crates.io/crates/opener
-        // This should open a file on windows/linux/macos
-        if let Some(show) = state.shows.get(&state.selected_key) {
+        if let Some(show) = state.shows.get_mut(&state.selected_key) {
             println!("Selected: {:?}", show);
+            println!("{}", show.current_episode().display());
+            // TODO: handle errors
+            opener::open(show.current_episode());
+            println!("Opened: {:?}", show.current_episode().display());
+            println!("Returning control back to pls");
+            //show.next();
         }
     };
 
