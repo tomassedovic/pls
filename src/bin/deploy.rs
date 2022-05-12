e72ca5a2107652eaba902d7587e93c558752a7d5
Tomas Sedovic <tomas@sedovic.cz>
2022-05-12 14:39:02 +0200

    Append the target triple to the release build
    The previous commit tried to upload the release binary with the target
    triple in the name but of course, it didn't actually exist.
    
    This time, we create it in the "deploy" script.

--- src/bin/deploy.rs
+++ src/bin/deploy.rs
@@ -7,10 +7,29 @@ fn main() -> anyhow::Result<()> {
     let rel = dir.join("target").join("release").canonicalize()?;
     println!("Release directory: {}", rel.display());
 
+    #[cfg(windows)]
+    let extension = "exe";
+    #[cfg(not(windows))]
     let extension = "";
 
     let release_file = rel.join("pls").with_extension(extension);
     println!("Release file: {}", release_file.display());
+
+    let release_suffix = std::env::var("TARGET_TRIPLE").unwrap_or_default();
+
+    let mut target_file_name = String::from("pls");
+    if !release_suffix.is_empty() {
+        target_file_name.push('-');
+        target_file_name.push_str(&release_suffix);
+    }
+    if !extension.is_empty() {
+        target_file_name.push_str(extension);
+    }
+
+    let target_file = release_file.with_file_name(target_file_name);
+    println!("Target file: {}", target_file.display());
+
+    std::fs::copy(&release_file, &target_file)?;
 
     Ok(())
 }
