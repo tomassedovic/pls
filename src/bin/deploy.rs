ff149214ce4cca1ed48f3beeaf86a0b10d8c30fd
Tomas Sedovic <tomas@sedovic.cz>
2022-05-21 22:49:51 +0200

    Implement creating the zip file in deploy
    We'll need to move it to the windows and macos section rather than
    producing it every time. But yep, we're building a zip file now too.

--- src/bin/deploy.rs
+++ src/bin/deploy.rs
@@ -1,4 +1,8 @@
-use std::path::Path;
+use std::{
+    fs::File,
+    io::Write,
+    path::{Path, PathBuf},
+};
 
 fn main() -> anyhow::Result<()> {
     println!("Hello, world!");
@@ -27,8 +31,99 @@ fn main() -> anyhow::Result<()> {
         .with_file_name(target_file_name)
         .with_extension(extension);
     println!("Target file: {}", target_file.display());
+    let source_file_name = release_file
+        .file_name()
+        .expect("Release file name is `None`")
+        .to_str()
+        .expect("Could not convert file name to a str.");
 
-    std::fs::copy(&release_file, &target_file)?;
+    #[cfg(target_os = "linux")]
+    {
+        use std::process::Command;
+
+        let publish_dir = std::path::PathBuf::from("target/publish");
+
+        if publish_dir.exists() {
+            println!(
+                "The publish directory exists, deleting: {}",
+                publish_dir.display()
+            );
+        }
+
+        println!("Creating the publish directory: {}", publish_dir.display());
+        std::fs::create_dir_all(&publish_dir)?;
+
+        println!(
+            "Copying release file \"{}\" to \"{}\"",
+            release_file.display(),
+            publish_dir.display()
+        );
+        std::fs::copy(&release_file, publish_dir.join(source_file_name))?;
+        println!("Copying readme to the publish dir.");
+        std::fs::copy("README.md", publish_dir.join("README.md"))?;
+        println!("Copying the license file to the publish dir.");
+        std::fs::copy("COPYING.txt", publish_dir.join("COPYING.txt"))?;
+
+        println!("Compressing the publish dir.");
+        let mut tarfile = target_file.clone();
+        tarfile.set_extension("tar.gz");
+        let tarfile = tarfile
+            .file_name()
+            .expect("Target file name is `None`.")
+            .to_str()
+            .expect("Could not convert the target file name to str.");
+
+        println!("Destination: {}", publish_dir.join(tarfile).display());
+        Command::new("tar")
+            .args([
+                "-czf",
+                tarfile,
+                "README.md",
+                "COPYING.txt",
+                source_file_name,
+            ])
+            .current_dir(publish_dir)
+            .output()
+            .expect("Failed to create the tar archive.");
+    }
+
+    // TODO: move this to the macos/windows target
+    {
+        use zip::{write::FileOptions, ZipWriter};
+        let method = zip::CompressionMethod::Stored;
+        let options = FileOptions::default()
+            .compression_method(method)
+            .unix_permissions(0o755);
+
+        let mut zip_path = target_file;
+        zip_path.set_extension("zip");
+        dbg!(&zip_path);
+        let zipfile = File::create(&zip_path)?;
+        let mut zip = zip::ZipWriter::new(zipfile);
+
+        {
+            use std::io::Read;
+            zip.start_file("README.md", options)?;
+            zip.write_all(std::fs::read_to_string("README.md")?.as_bytes())?;
+
+            zip.start_file("COPYING.txt", options)?;
+            zip.write_all(std::fs::read_to_string("COPYING.txt")?.as_bytes())?;
+
+            zip.start_file(source_file_name, options)?;
+            let mut f = File::open(release_file)?;
+            let mut buf = Vec::new();
+            f.read_to_end(&mut buf)?;
+            zip.write_all(&buf)?;
+
+            zip.finish()?;
+        }
+    }
+
+    #[cfg(any(target_os = "macos", target_os = "windows"))]
+    {
+        // TODO: create a zip file
+        println!("Compressing on windows or macos");
+    }
 
     Ok(())
 }
