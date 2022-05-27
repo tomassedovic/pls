3e4928e336bf21d823ee4b900ba49a3c374f887e
Tomas Sedovic <tomas@sedovic.cz>
2022-05-27 17:02:22 +0200

    Package the Mac OS app bundle in the deploy script
    This is a pretty big change to the deploy script. It now creates both
    zipfiles and tarballs using the relevant Rust crates (tar and zip),
    it's able to package the .app bundle (which is secretly a directory)
    and we take all the arguments form the environment rather than doing
    platform detection logic in the script.
    
    The code could use some cleanup, but it does in fact seem to do
    everything that's currently needed.

--- src/bin/deploy.rs
+++ src/bin/deploy.rs
@@ -1,128 +1,135 @@
 use std::{
     fs::File,
-    io::Write,
+    io::{Read, Seek, Write},
     path::{Path, PathBuf},
 };
 
-fn main() -> anyhow::Result<()> {
-    println!("Hello, world!");
-    let dir = Path::new(".").canonicalize()?;
-    println!("Current directory: {}", dir.display());
-    let rel = dir.join("target").join("release").canonicalize()?;
-    println!("Release directory: {}", rel.display());
+use zip::ZipWriter;
 
-    #[cfg(windows)]
-    let extension = "exe";
-    #[cfg(not(windows))]
-    let extension = "";
-
-    let release_file = rel.join("pls").with_extension(extension);
-    println!("Release file: {}", release_file.display());
-
-    let release_suffix = std::env::var("TARGET_TRIPLE").unwrap_or_default();
-
-    let mut target_file_name = String::from("pls");
-    if !release_suffix.is_empty() {
-        target_file_name.push('-');
-        target_file_name.push_str(&release_suffix);
-    }
-
-    let target_file = release_file
-        .with_file_name(target_file_name)
-        .with_extension(extension);
-    println!("Target file: {}", target_file.display());
-    let source_file_name = release_file
-        .file_name()
-        .expect("Release file name is `None`")
-        .to_str()
-        .expect("Could not convert file name to a str.");
-
-    #[cfg(target_os = "linux")]
-    {
-        use std::process::Command;
-
-        let publish_dir = std::path::PathBuf::from("target/publish");
-
-        if publish_dir.exists() {
-            println!(
-                "The publish directory exists, deleting: {}",
-                publish_dir.display()
-            );
+fn zip_add_directory<T: Write + Seek>(
+    zip: &mut ZipWriter<T>,
+    p: &Path,
+    dirname: &str,
+    options: zip::write::FileOptions,
+) -> anyhow::Result<()> {
+    println!("Adding directory: {} as: {}", p.display(), dirname);
+    let mut buffer = vec![];
+    for entry in walkdir::WalkDir::new(p) {
+        let entry = entry?;
+        if entry.path() == p {
+            continue;
         }
-
-        println!("Creating the publish directory: {}", publish_dir.display());
-        std::fs::create_dir_all(&publish_dir)?;
-
-        println!(
-            "Copying release file \"{}\" to \"{}\"",
-            release_file.display(),
-            publish_dir.display()
-        );
-        std::fs::copy(&release_file, publish_dir.join(source_file_name))?;
-        println!("Copying readme to the publish dir.");
-        std::fs::copy("README.md", publish_dir.join("README.md"))?;
-        println!("Copying the license file to the publish dir.");
-        std::fs::copy("COPYING.txt", publish_dir.join("COPYING.txt"))?;
-
-        println!("Compressing the publish dir.");
-        let mut tarfile = target_file.clone();
-        tarfile.set_extension("tar.gz");
-        let tarfile = tarfile
-            .file_name()
-            .expect("Target file name is `None`.")
-            .to_str()
-            .expect("Could not convert the target file name to str.");
-
-        println!("Destination: {}", publish_dir.join(tarfile).display());
-        Command::new("tar")
-            .args([
-                "-czf",
-                tarfile,
-                "README.md",
-                "COPYING.txt",
-                source_file_name,
-            ])
-            .current_dir(publish_dir)
-            .output()
-            .expect("Failed to create the tar archive.");
+        let stripped = entry.path().strip_prefix(p)?;
+        let stripped = PathBuf::from(dirname).join(stripped);
+        if entry.file_type().is_file() {
+            println!("Adding file: {}", stripped.display());
+            let mut f = File::open(entry.path())?;
+            f.read_to_end(&mut buffer)?;
+            let pname = format!("{}", stripped.display());
+            zip.start_file(&pname, options)?;
+            zip.write_all(&buffer)?;
+            buffer.clear();
+        }
     }
+    Ok(())
+}
 
-    // TODO: move this to the macos/windows target
-    {
-        use zip::{write::FileOptions, ZipWriter};
+fn main() -> anyhow::Result<()> {
+    let release_dir = Path::new("target").join("release").canonicalize()?;
+    println!("Release directory: {}", release_dir.display());
+
+    let executable_extension = std::env::var("EXECUTABLE_EXTENSION").unwrap_or_default();
+    let archive_extension = std::env::var("ARCHIVE_EXT").unwrap_or_else(|_| String::from("zip"));
+    let release_suffix = std::env::var("TARGET_TRIPLE").unwrap_or_default();
+    let cargo_bundle = std::env::var("CARGO_BUNDLE").unwrap_or_default() == "true";
+
+    let app_build = if cargo_bundle {
+        // TODO: handle all bundles, not just the macos app one
+        release_dir.join("bundle/osx/pls.app")
+    } else {
+        release_dir
+            .join("pls")
+            .with_extension(&executable_extension)
+    };
+    let files_to_package = vec![
+        PathBuf::from("COPYING.txt"),
+        PathBuf::from("README.md"),
+        app_build,
+    ];
+
+    let archive_filename = {
+        let mut s = String::from("pls");
+        if !release_suffix.is_empty() {
+            s.push('-');
+            s.push_str(&release_suffix);
+        }
+        s.push('.');
+        s.push_str(&archive_extension);
+        s
+    };
+    println!("Archive filename: {}", &archive_filename);
+
+    if archive_extension == "zip" {
         let method = zip::CompressionMethod::Stored;
-        let options = FileOptions::default()
+        let options = zip::write::FileOptions::default()
             .compression_method(method)
             .unix_permissions(0o755);
 
-        let mut zip_path = target_file;
-        zip_path.set_extension("zip");
-        dbg!(&zip_path);
-        let zipfile = File::create(&zip_path)?;
-        let mut zip = zip::ZipWriter::new(zipfile);
+        let zipfile = File::create(&archive_filename)?;
+        let mut zip = ZipWriter::new(zipfile);
 
-        {
-            use std::io::Read;
-            zip.start_file("README.md", options)?;
-            zip.write_all(std::fs::read_to_string("README.md")?.as_bytes())?;
+        let mut buf = Vec::new();
+        for p in &files_to_package {
+            println!("Adding file to the archive: {}", p.display());
+            let filename = p
+                .file_name()
+                .expect("No file name in path.")
+                .to_str()
+                .expect("Cannot convert target file to str.");
 
-            zip.start_file("COPYING.txt", options)?;
-            zip.write_all(std::fs::read_to_string("COPYING.txt")?.as_bytes())?;
-
-            zip.start_file(source_file_name, options)?;
-            let mut f = File::open(release_file)?;
-            let mut buf = Vec::new();
-            f.read_to_end(&mut buf)?;
-            zip.write_all(&buf)?;
-
-            zip.finish()?;
+            // NOTE: A macos app file is in fact a directory
+            if p.is_file() {
+                let mut f = File::open(p)?;
+                zip.start_file(filename, options)?;
+                f.read_to_end(&mut buf)?;
+                zip.write_all(&buf)?;
+            } else if p.is_dir() {
+                zip_add_directory(&mut zip, p, filename, options)?;
+            } else {
+                anyhow::bail!("Path is neither file nor directory: '{}'", p.display());
+            }
         }
+        zip.finish()?;
+    } else if archive_extension == "tar.gz" {
+        use flate2::{write::GzEncoder, Compression};
+
+        let tarfile = File::create(&archive_filename)?;
+        let encoder = GzEncoder::new(tarfile, Compression::default());
+        let mut tar = tar::Builder::new(encoder);
+        for p in &files_to_package {
+            println!("Adding file to the archive: {}", p.display());
+            let filename = p
+                .file_name()
+                .expect("No file name in path.")
+                .to_str()
+                .expect("Cannot convert target file to str.");
+
+            // TODO: handle directories.
+            let mut f = File::open(p)?;
+            tar.append_file(filename, &mut f)?;
+        }
+    } else {
+        anyhow::bail!("Unsupported archive extension: '{archive_extension}'");
     }
 
-    #[cfg(any(target_os = "macos", target_os = "windows"))]
-    {
-        // TODO: create a zip file
-        println!("Compressing on windows or macos");
+    let archive_path = Path::new(&archive_filename);
+    if archive_path.is_file() {
+        println!("Created archive in: {}", archive_path.display());
+    } else {
+        anyhow::bail!(
+            "Archive '{}' was not created successfully.",
+            archive_path.display()
+        )
     }
 
     Ok(())
